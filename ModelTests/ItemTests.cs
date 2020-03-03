cdc5b8177e00c92378c4be93ae95e2f969bc5956
Student <example@example.com>
2020-03-03 15:58:03 -0800

    create function to string together coin amounts needed

--- ModelTests/ItemTests.cs
+++ ModelTests/ItemTests.cs
@@ -10,10 +10,27 @@ namespace CoinCombination.Tests
   public class CoinCombinationTests
   {
     [TestMethod]
-    public void CreateCoin_CreateInstance_Coin()
+    public void Coin_CreateInstanceOfCoinClass_Coin()
     {
-    Coin newCoin = new Coin();
+    Coin newCoin = new Coin(1);
     Assert.AreEqual(typeof(Coin), newCoin.GetType());
+    }
+
+    [TestMethod]
+    public void Coin_SetValueOfCoinAmountInCoinClass_double()
+    {
+      double amount = .50;
+      Coin newCoin = new Coin(amount);
+      Assert.AreEqual(.50, amount);
+    }
+
+    [TestMethod]
+    public void Coin_AccountForNumberOfCoins_double()
+    {
+      double amount = .80;
+      Coin newCoin = new Coin(amount);
+      newCoin.CountChange();
+      Assert.AreEqual(5, amount);
     }
   }
 }
